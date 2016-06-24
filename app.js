d3bf4fc4ef9cbe3c4764360ca70652c3eee25edf
FernAbby <linkjanew@gmail.com>
2016-06-24 14:46:56 +0800

    修复req.session undefined Bug
    解决方法：把路由放到session后面，调整顺序

--- app.js
+++ app.js
@@ -9,18 +9,14 @@ var session = require('express-session');
 var flash = require('connect-flash');
 
 var app = express();
-
-//路由
 var route = require('./routes/route');
-route(app);
 
 //设置模版引擎
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(flash());
 app.use(session({
@@ -29,6 +25,10 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+
+//路由
+route(app);
+
 //设置静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req,res,next){
--- routes/index.js
+++ routes/index.js
@@ -4,58 +4,56 @@ var router = express.Router();
 
 var User = require('../models/user.js');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home', { title: '首页'});
+router.get('/', function (req, res, next) {
+    res.render('home', {title: '首页'});
 });
-router.get('/reg',function(req, res, next){
-  res.render('reg',{title: '用户注册'});
+router.get('/reg', function (req, res, next) {
+    res.render('reg', {title: '用户注册'});
 });
-router.post('/reg',function(req,res,next){
-  req.on('data',function(data){
-    var dataJson = querystring.parse(data.toString());
-    var account = dataJson['account'],
-        password = dataJson['password'],
-        passwordRepeat = dataJson['passwordRepeat'];
-    if(password != passwordRepeat){
-      res.json({
-        code: 1,
-        msg: '两次输入密码不一致'
-      });
-      return;
-    }
-    var newUser = new User({
-      account: account,
-      password: password
-    });
-    User.get(newUser.account,function(err,user){
-      if(user){
-        err = {
-          code: 1,
-          msg: '用户名已存在'
-        };
-      }
-      if(err){
-        res.json(err);
-        return;
-      }
-      newUser.save(function(err,user){
-        if (err) {
-          res.json(err);
-          return;
-        }
-        console.log(user);
-        //req.session.user = user;
+router.post('/reg', function (req, res, next) {
+    console.log(req.body);
+    var account = req.body['account'],
+        password = req.body['password'],
+        passwordRepeat = req.body['passwordRepeat'];
+    if (password != passwordRepeat) {
         res.json({
-          code: 0,
-          msg: '注册成功!'
+            code: 1,
+            msg: '两次输入密码不一致'
         });
         return;
-      });
+    }
+    var newUser = new User({
+        account: account,
+        password: password
     });
-  });
+    User.get(newUser.account, function (err, user) {
+        if (user) {
+            err = {
+                code: 1,
+                msg: '用户名已存在'
+            };
+        }
+        if (err) {
+            res.json(err);
+            return;
+        }
+        newUser.save(function (err, user) {
+            if (err) {
+                res.json(err);
+                return;
+            }
+            req.session.user = user;
+            res.json({
+                code: 0,
+                msg: '注册成功!'
+            });
+            return;
+        });
+    });
 });
-router.get('/login',function( req, res, next){
-  res.render('login',{title: '用户登录'});
+router.get('/login', function (req, res, next) {
+    console.log(req.body);
+    res.render('login', {title: '用户登录'});
 });
 
 module.exports = router;
